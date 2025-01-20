22a837333eeb938c720e73715e5c88403f05ec5b
YOGI MEIZAL PUTRA <yogimeizalputra@gmail.com>
2025-01-20 13:55:37 +0700

    Setup Fonts and Assets

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className="font-bold text-lg my-10">Welcome to Rumahkoe</Text>
+      <Text className="font-bold my-10 font-rubik text-3xl">Welcome to Rumahkoe</Text>
       <Link href="../sign-in">Sign In</Link>
       <Link href="./explore">Explore</Link>
       <Link href="./profile">Profile</Link>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,27 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import "./global.css";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
+    "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+    "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+    "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf")
+  })
+
+  useEffect(() => {
+    if (fontsLoaded) { 
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return <Stack />;
 }
